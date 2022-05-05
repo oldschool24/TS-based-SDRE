46ac61f67fe4944e39aa66bfdf2103be1ad9d149
Puzach Vladislav <puzachvladis@gmail.com>
2022-05-05 16:12:19 +0300

    updated mainSim

--- mainSim.m
+++ mainSim.m
@@ -1,6 +1,6 @@
 function mainSim(sysRhs, learnStep, T, tsModel)
     arguments
-        sysRhs = @rhsMotorLink;
+        sysRhs = @sys.rhsMotorLink;
         learnStep = 0.01;
         T = 20;
         tsModel = readfis('models/motor_link.fis'); 
@@ -9,17 +9,31 @@ function mainSim(sysRhs, learnStep, T, tsModel)
     u = @(x) tsBasedControl(x, tsModel, learnStep);
     timesteps = 0:learnStep:T;
     x0 = [-0.4; 2];  % change it sometime
+    global fTrue fPred Btrue Bpred
     [~, X_true] = ode45(@(t, x) sysRhs(x, u(x)), timesteps, x0);
     
     figure()
     plot(timesteps, X_true)
     legend('x1', 'x2')
-end
 
-function dXdt = rhsMotorLink(x, u)
-% right hand size of motor link system
-    dXdt = zeros(2, 1);
-    dXdt(1) = x(2);
-    dXdt(2) = -64*sin(x(1)) - 5*x(2) + 400*u;
-end
+    L = length(fTrue);
+    figure()
+    title('f1')
+    plot(1:L, fTrue(:, 1), 1:L, fPred(:, 1))
+    legend('f_1', 'f_1Pred')
 
+    figure()
+    title('f2')
+    plot(1:L, fTrue(:, 2), 1:L, fPred(:, 2))
+    legend('f_2', 'f_2Pred')
+
+    figure()
+    title('B1')
+    plot(1:L, Btrue(:, 1), 1:L, Bpred(:, 1))
+    legend('B_1', 'B_1Pred')
+
+    figure()
+    title('B2')
+    plot(1:L, Btrue(:, 2), 1:L, Bpred(:, 2))
+    legend('B_2', 'B_2Pred')
+end
