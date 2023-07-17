307fb129229f5f4b93752d138be2b38a48bd7ae6
Puzach Vladislav <puzachvladis@gmail.com>
2023-07-17 17:05:52 +0600

    added new way of A estimation

--- tsBasedControl.m
+++ tsBasedControl.m
@@ -76,6 +76,7 @@ function [u, fHat, hatB, errorFlag] = tsBasedControl( ...
 %         stabilizable(hatA, hatB) && detectable(hatA, sqrtm(Q))  % check
     else
         hatA = 1/dt * (waveA - eye(n));
+%         hatA = weightedA(dt, waveA, x_pure, 'perElement');
         [hatA, hatB] = knownChange(sysName, known, x_pure, hatA, hatB);
         [P, ~, ~, info] = icare(hatA, hatB, Q, R);
     end
@@ -84,34 +85,12 @@ function [u, fHat, hatB, errorFlag] = tsBasedControl( ...
     % 4. Calculate u = SDRE(hatA, hatB)
     if isempty(P)
         disp('Solution of SDRE has not been found')
-        Dx = diag(info.Sx);
-        P = Dx * info.V * inv(info.U) * Dx;
-%         u = zeros(r, 1);
+%         Dx = diag(info.Sx);
+%         P = Dx * info.V * inv(info.U) * Dx;
+        u = zeros(r, 1);
         errorFlag = true;
-    end
-    u = -inv(R) * hatB' * P * x;  % TODO: x_pure instead x?
-end
-
-function choices = getChoices(maxNum, nonzeroIdxs, zeroIdxs)
-    % choice = join(permutation(nonzeroIdxs), combination(nonzeroIdxs))
-    nZero = numel(zeroIdxs);
-    permutations = perms(nonzeroIdxs);  % without repetitions
-    nPerm = size(permutations, 1);
-
-    if nZero == 0
-        nChoices = min(maxNum, nPerm);
-        choices = datasample(1:nPerm, nChoices, 'Replace', false);
-        choices = permutations(choices, :);
     else
-        combinations = randsample(nonzeroIdxs, 10*nZero, true);
-        combinations = reshape(combinations, [10, nZero]);
-        nComb = size(combinations, 1);
-    
-        [permIdxs, combIdxs] = ndgrid(1:nPerm, 1:nComb);
-        nChoices = min(maxNum, numel(permIdxs));
-        choices = datasample(1:numel(permIdxs), nChoices, 'Replace', false);
-        choices = [permutations(permIdxs(choices), :), ...
-                   combinations(combIdxs(choices), :)];
+        u = -inv(R) * hatB' * P * x;  % TODO: x_pure instead x?
     end
 end
 
@@ -122,146 +101,6 @@ function [hatA, hatB] = knownChange(sysName, known, x_pure, hatA, hatB)
         hatA(known, :) = A(known, :);
         hatB(known, :) = B(known, :);
     end
-end
-
-function isHold = stabilizable(A, B)
-    isHold = true;  % has full row rank for all RE(lamda)>=0
-    [n, ~] = size(A);
-    eigValues = eig(A);
-    eigValues(real(eigValues) < 0) = []; % only Re(lambda)>=0
-    for iValue=1:length(eigValues)
-        lambda = eigValues(iValue);
-        if rank([A - lambda*eye(n), B]) ~= n
-            isHold = false;
-            break
-        end
-    end
-end
-
-function isHold = detectable(A, C)
-    isHold = true;  % has full column rank for all RE(lamda)>=0
-    [~, n] = size(A);
-    eigValues = eig(A);
-    eigValues(real(eigValues) < 0) = []; % only Re(lambda)>=0
-    for iValue=1:length(eigValues)
-        lambda = eigValues(iValue);
-        if rank([A - lambda*eye(n); C]) ~= n
-            isHold = false;
-            break
-        end
-    end
-end
-
-function outputArray = reorderRows(choices, f, x, ...
-                                   nNZ, almostZeroIdxs, zeroIdxs)
-    arrNorm = zeros(size(choices, 1), 1);
-    for iRow = 1:size(choices, 1)
-        % Calculate norm of matrix A
-        hatA = choice2hatA(choices(iRow, :), f, x, ...
-                           nNZ, almostZeroIdxs, zeroIdxs);
-        arrNorm(iRow) = norm(hatA);
-    end
-    
-    % Sort the rows based on the number of unique elements
-    % if equal, take the row corresponding to the smaller matrix norm 
-    [~, sortOrder] = sortrows(arrNorm, 'ascend'); 
-    
-    % Reorder the rows based on the sorting order
-    outputArray = choices(sortOrder, :);  
-end
-
-function hatA = choice2hatA(choice, f, x, nNZ, almostZeroIdxs, zeroIdxs)
-    n = numel(x);
-    nAZ = numel(almostZeroIdxs); 
-    hatA = zeros(n);
-    for row=1:n
-        if row > nNZ
-            if row <= nNZ+nAZ
-                % A(row,k)*x_k + A(row,s)*x_s = f(row)
-                % A(row,k)=-1 => A(row,s) = (f(row)+x_k) / x_s
-                column = almostZeroIdxs(row-nNZ);
-                f(row) = f(row) + x(column);
-            else
-                % fill in the elements that:
-                % 1. affect the stabilizability and detectability
-                % 2. do not affect hatA * x_pure
-                column = zeroIdxs(row-nNZ-nAZ);
-            end
-            hatA(row, column) = -1;
-        end
-        column = choice(row);
-        hatA(row, column) = f(row) / x(column);
-    end
-end
-
-function [hatA, P, info] = unwrappedAfromWrapped(dt, waveA, x_processed, ...
-                                                 x_pure, Q, R)
-    n = numel(x_processed);
-    usual_f_hat = 1/dt * (waveA - eye(n)) * x_processed;
-    % must find such hatA: usual_f_hat = hatA * pure_x
-    % n equations, n^2 elements in A => choose n elements of A
-    % a_ik is candidate, if pure_x(k) ~= 0
-    nonzeroIdxs = find(abs(x_pure) > 1e-6);
-    almostZeroIdxs = find(abs(x_pure) < 1e-6 & x_pure);
-    zeroIdxs = setdiff(setdiff(1:n, nonzeroIdxs), almostZeroIdxs);
-    nNZ = numel(nonzeroIdxs);
-    maxNumChoices = 100;
-    choices = getChoices(maxNumChoices, nonzeroIdxs, ...
-                         union(almostZeroIdxs, zeroIdxs));
-    choices = reorderRows(choices, usual_f_hat, x_pure, ...
-                          nNZ, almostZeroIdxs, zeroIdxs); 
-
-%         nonzeroND = cell(nNZ, 1);
-%         nonzeroND(:) = {nonzeroIdxs};  % every cell contains nonzeroIdxs
-%         combs = cell(nNZ, 1);
-%         [combs{:}] = ndgrid(nonzeroND{:}); % combs(:, k) = valid choice
-    
-%         nChoices = min(300, numel(combs{1}));
-%         combsArr = zeros(nChoices, nNZ);
-%         iArr = 1;
-%         for iChoice=datasample(1:numel(combs{1}), nChoices, 'Replace', false)
-%             for k=1:nNZ
-%                 combsArr(iArr, k) = combs{k}(iChoice);
-%             end
-%             iArr = iArr + 1;
-%         end
-
-    sqrtmQ = sqrtm(Q);
-    nChoices = size(choices, 1);
-    for iChoice=1:nChoices  % TODO: continuity?
-        hatA = choice2hatA(choices(iChoice, :), usual_f_hat, x_pure,...
-                           nNZ, almostZeroIdxs, zeroIdxs);
-        [hatA, hatB] = knownChange(sysName, known, x_pure, hatA, hatB);
-        if stabilizable(hatA, hatB) && detectable(hatA, sqrtmQ)
-            % then unique positive semi-definite solution exists
-            % sum(usual_f_hat - hatA * x_pure, 'all')
-            [P, ~, ~, info] = icare(hatA, hatB, Q, R);
-            break
-        end
-    end
-    if iChoice == nChoices
-        [P, ~, ~, info] = icare(hatA, hatB, Q, R);
-    end
-
-%         selected = randsample(nonzeroIdxs, n, true);
-%         hatA = zeros(n);
-%         for k=1:n
-%             column = selected(k);
-%             row = k;
-%             hatA(row, column) = usual_f_hat(row) / x_pure(column);
-%         end
-
-%         hatA = diag(usual_f_hat ./ x_pure);
-%         infIdxs = isinf(hatA);
-%         if any(infIdxs, 'all')
-%             hatA(infIdxs) = 0;
-%             options = optimset('MaxFunEvals', 200*(n^2), ...
-%                                'MaxIter', 200*(n^2), ...
-%                                'TolFun', 1e-3, 'TolX', 1e-3);
-%             [hatA, ~, ~, optInfo] = fminsearch( ...
-%                 @(A) norm(usual_f_hat - A*x_pure), hatA, options);
-%             disp(optInfo)
-%         end
 end
 
 function [hatA, P, info] = minimalAndSimilarA(dt, waveA, x_processed, ...
@@ -291,4 +130,63 @@ function [hatA, P, info] = minimalAndSimilarA(dt, waveA, x_processed, ...
 %     sum(1/dt*(waveA-eye(n))*x_processed - hatA*x_pure, 'all')  % check 
 
     [P, ~, ~, info] = icare(hatA, hatB, Q, R);
+end
+
+function hatA = weightedA(dt, waveA, x, type)
+    n = numel(x);
+    fHat = 1/dt * (waveA-eye(n)) * x;
+    
+    if strcmp(type, 'perMatrix')
+        candidates = zeros(n, n, n+1);
+        for iCandidate=1:n
+            A = zeros(n, n);
+            if abs(x(iCandidate)) > 1e-9
+                A(:, iCandidate) = fHat ./ x(iCandidate);
+            end
+            candidates(:, :, iCandidate) = A;
+        end
+        candidates(:, :, n+1) = 1/dt * (waveA-eye(n));   
+
+        Aeq = ones(1, n+1);  % sum(w) = 1
+        beq = 1;
+        lb = zeros(n+1, 1);  % 0 <= w <= 1
+        ub = ones(n+1, 1);
+    elseif strcmp(type, 'perElement')
+        candidates = fHat ./ x';  % a_ij = f_i / x_j
+        [ii, jj] = find(isinf(candidates));
+        linIdxs = sub2ind([n, n], ii, jj);
+        candidates(isinf(candidates)) = 10^12;  % zero leads to Inf
+        
+        Aeq = repmat(eye(n), 1, n);
+%         Aeq(:, linIdxs) = 0;
+        beq = ones(n, 1);
+        lb = zeros(n^2, 1);
+        ub = ones(n^2, 1);
+%         ub(linIdxs) = 0;
+    end
+   
+    global w_alpha; % [zeros(n, 1); 1]
+    wOpt = fmincon(@(w) bestFactorizationObjective(w, candidates, type), ...
+                  w_alpha, [], [], Aeq, beq, lb, ub, [], ...
+                  optimoptions('fmincon', 'Display', 'off', ...
+                  'OptimalityTolerance', 1e-6, 'MaxIterations', 10, ...
+                  'ConstraintTolerance', 1e-6));
+    w_alpha = wOpt;
+
+    if strcmp(type, 'perMatrix')
+        hatA = sum(candidates .* reshape(wOpt, 1, 1, []), 3);
+    elseif strcmp(type, 'perElement')
+        hatA = candidates .* reshape(wOpt, size(candidates));
+    end
+%     fHat - hatA * x
+end
+
+function value = bestFactorizationObjective(w, candidates, type)
+    if strcmp(type, 'perMatrix')
+        weighted = sum(candidates .* reshape(w, 1, 1, []), 3);
+    elseif strcmp(type, 'perElement')
+        matrW = reshape(w, size(candidates));
+        weighted = matrW .* candidates;
+    end
+    value = norm(weighted) * norm(inv(weighted));
 end
